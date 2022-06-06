cb8985455e14e7dee5566bd1d9dfc8c8ce593597
Abin Rapheal <86885009+rapheljr@users.noreply.github.com>
2022-06-06 17:59:55 +0530

    Added phone number and address

--- src/form.js
+++ src/form.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 
 class Form {
   constructor() {
-    this.questions = ['name', 'DOB', 'hobbies'];
+    this.questions = ['name', 'DOB', 'hobbies',
+      'phone number', 'address line 1', 'address line 2'];
     this.current = 0;
   }
 
@@ -26,6 +27,14 @@ class Form {
     return false;
   }
 
+  addPhone(number) {
+    if (validatePhone(number)) {
+      this.phone = number;
+      return true;
+    }
+    return false;
+  }
+
   addHobbies(hobbies) {
     if (validateHobbies(hobbies)) {
       this.hobbies = hobbies.split(',');
@@ -34,18 +43,35 @@ class Form {
     return false;
   }
 
+  addAddressLine1(line1) {
+    this.address = line1;
+    return true;
+  }
+
+  addAddressLine2(line2) {
+    this.address += '\n' + line2;
+    return true;
+  }
+
   question() {
     const something = this.questions[this.current];
     console.log('Please enter your ' + something + ': ');
   }
 
   getDetails() {
-    return { name: this.name, DOB: this.DOB, hobbies: this.hobbies };
+    return {
+      name: this.name, DOB: this.DOB, hobbies: this.hobbies,
+      phone: this.phone, address: this.address
+    };
   }
 
 }
 
 const validateName = (name) => name.length > 4;
+
+const validatePhone = (number) => {
+  return isFinite(number) && ('' + number).length === 10;
+};
 
 const validateDOB = (DOB) => DOB.length === 10;
 
@@ -60,15 +86,15 @@ const write = (json) => {
 const main = () => {
   const form = new Form();
   form.question();
-  const questions = [form.addName, form.addDOB, form.addHobbies];
+  const methods = ['addName', 'addDOB', 'addHobbies',
+    'addPhone', 'addAddressLine1', 'addAddressLine2'];
   let index = 0;
   process.stdin.setEncoding('utf-8');
   process.stdin.on('data', (chunk) => {
-    console.log(form.getDetails());
-    if (questions[index](chunk.slice(0, -1))) {
+    if (form[methods[index]](chunk.slice(0, -1))) {
       form.next();
       index++;
-      if (index === questions.length) {
+      if (index === methods.length) {
         write(JSON.stringify(form.getDetails()));
       }
     }
